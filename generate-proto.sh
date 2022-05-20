8b9b4812c725139a584ee2e80d025893062f926a
fabian <fabian.lange@uni-potsdam.de>
2022-05-21 01:28:22 +0200

    Generate protobuf schema for union

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=proto --python_out=build/gen proto/corporate/bafin/v1/corporate.proto
+protoc --proto_path=proto --python_out=build/gen proto/bakdata/union/v1/union.proto
