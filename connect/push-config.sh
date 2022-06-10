8966d7f269952fe9625419e299760f88b8d0441e
fabian <fabian.lange@uni-potsdam.de>
2022-06-09 19:26:13 -0700

    Write connectors to enable search on elasticsearch for each schema

--- connect/push-config.sh
+++ connect/push-config.sh
@@ -2,8 +2,23 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+
+BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink-bafin_event.json"}
+BASE_CONFIG2=${3:-"$(dirname $0)/elastic-sink-bafin_corporate.json"}
+BASE_CONFIG3=${3:-"$(dirname $0)/elastic-sink-bafin_person.json"}
+BASE_CONFIG4=${3:-"$(dirname $0)/elastic-sink-rb_corporate.json"}
+BASE_CONFIG5=${3:-"$(dirname $0)/elastic-sink-rb_person.json"}
+
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
 data=$(cat $BASE_CONFIG | jq -s '.[0]')
+data2=$(cat $BASE_CONFIG2 | jq -s '.[0]')
+data3=$(cat $BASE_CONFIG3 | jq -s '.[0]')
+data4=$(cat $BASE_CONFIG4 | jq -s '.[0]')
+data5=$(cat $BASE_CONFIG5 | jq -s '.[0]')
+
 curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
+curl -X POST $KAFKA_CONNECT_API --data "$data2" -H "content-type:application/json"
+curl -X POST $KAFKA_CONNECT_API --data "$data3" -H "content-type:application/json"
+curl -X POST $KAFKA_CONNECT_API --data "$data4" -H "content-type:application/json"
+curl -X POST $KAFKA_CONNECT_API --data "$data5" -H "content-type:application/json"
