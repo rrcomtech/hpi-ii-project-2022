10b457e81e68ffe4abaaf9fb749f0b12d934efe7
fabian <fabian.lange@uni-potsdam.de>
2022-06-12 07:55:54 -0700

    Add connector for union schema

--- connect/push-config.sh
+++ connect/push-config.sh
@@ -8,6 +8,7 @@ BASE_CONFIG2=${3:-"$(dirname $0)/elastic-sink-bafin_corporate.json"}
 BASE_CONFIG3=${3:-"$(dirname $0)/elastic-sink-bafin_person.json"}
 BASE_CONFIG4=${3:-"$(dirname $0)/elastic-sink-rb_corporate.json"}
 BASE_CONFIG5=${3:-"$(dirname $0)/elastic-sink-rb_person.json"}
+BASE_CONFIG6=${3:-"$(dirname $0)/elastic-sink-union.json"}
 
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
@@ -16,9 +17,11 @@ data2=$(cat $BASE_CONFIG2 | jq -s '.[0]')
 data3=$(cat $BASE_CONFIG3 | jq -s '.[0]')
 data4=$(cat $BASE_CONFIG4 | jq -s '.[0]')
 data5=$(cat $BASE_CONFIG5 | jq -s '.[0]')
+data6=$(cat $BASE_CONFIG6 | jq -s '.[0]')
 
 curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
 curl -X POST $KAFKA_CONNECT_API --data "$data2" -H "content-type:application/json"
 curl -X POST $KAFKA_CONNECT_API --data "$data3" -H "content-type:application/json"
 curl -X POST $KAFKA_CONNECT_API --data "$data4" -H "content-type:application/json"
 curl -X POST $KAFKA_CONNECT_API --data "$data5" -H "content-type:application/json"
+curl -X POST $KAFKA_CONNECT_API --data "$data6" -H "content-type:application/json"
