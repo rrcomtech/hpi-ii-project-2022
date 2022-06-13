21b586947ec92cf9ed84195c14c2bb09255cada7
Robert Richter <robert.richter@rrcomtech.com>
2022-06-13 10:26:55 +0200

    Merge

--- union_generator/constant.py
+++ union_generator/constant.py
@@ -1,4 +1,4 @@
 BOOTSTRAP_SERVER: str = "localhost:29092"
 SCHEMA_REGISTRY_URL: str = "http://localhost:8081"
-TOPIC: str = "union-events"
+TOPIC_UNION: str = "union-events"
 GROUP_ID: str = "bafin-consumers"
--- union_generator/union_consumer.py
+++ union_generator/union_consumer.py
@@ -7,7 +7,9 @@ from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.protobuf import ProtobufDeserializer
 from confluent_kafka.serialization import StringDeserializer
 
-from union_generator.constant import BOOTSTRAP_SERVER, GROUP_ID, SCHEMA_REGISTRY_URL
+from general.producer import Producer
+
+from union_generator.constant import BOOTSTRAP_SERVER, GROUP_ID, SCHEMA_REGISTRY_URL, TOPIC_UNION
 from rb_crawler.constant import TOPIC_RB_CORPORATE, TOPIC_RB_PERSON
 from rights_bafin_crawler.constant import TOPIC_BAFIN_CORPORATE, TOPIC_BAFIN_PERSON, TOPIC_BAFIN
 
@@ -17,6 +19,7 @@ from build.gen.bakdata.bafin.v1.bafin_person_pb2 import Bafin_Reportable_Person
 
 from build.gen.bakdata.rb.v1.rb_corporate_pb2 import RB_Corporate, RB_Status
 from build.gen.bakdata.rb.v1.rb_person_pb2 import RB_Person
+from build.gen.bakdata.union.v1.union_pb2 import Union, Union_Bafin_detail
 
 
 log = logging.getLogger(__name__)
@@ -28,10 +31,30 @@ class UnionConsumer:
         msg_integrated = {}
 
         msg_bafin_events = self.consume_topic(TOPIC_BAFIN, Bafin_Issuer)
+        print("Bafin-Events consumed.")
         msg_bafin_persons = self.consume_topic(TOPIC_BAFIN_PERSON, Bafin_Reportable_Person)
+        print("Bafin-Persons consumed.")
         msg_bafin_corporate = self.consume_topic(TOPIC_BAFIN_CORPORATE, Bafin_Reportable_Corp)
+        print("Bafin-Coporates consumed.")
         msg_rb_corporate = self.consume_topic(TOPIC_RB_CORPORATE, RB_Corporate)
+        print("HRB-Corporates consumed.")
         msg_rb_persons = self.consume_topic(TOPIC_RB_PERSON, RB_Person)
+        print("HRB-Persons consumed.")
+
+        for bafin_event in msg_bafin_events:
+            msg_integrated[bafin_event.issuer] = Union()
+            msg_integrated[bafin_event.issuer].corporateName = bafin_event.issuer
+            msg_integrated[bafin_event.issuer].bafin_id = str(bafin_event.issuer_id)
+            msg_integrated[bafin_event.issuer].bain_domicile = bafin_event.domicile
+            msg_integrated[bafin_event.issuer].bafin_country = bafin_event.country
+
+        for bafin_person in msg_bafin_persons:
+            msg_integrated[bafin_person.issuer].bafin_detail = None
+
+        print("Producing to Kafka ...")
+        producer = Producer(Union, TOPIC_UNION)
+        for union in msg_integrated:
+            producer.produce_to_topic(union, union.bafin_id)
 
         # Example for using the values:
         # print(str(msg_bafin_events[0].issuer))
