bc6caee4216944557bb56b8dfdff08eef28fc0bb
fabian <fabian.lange@uni-potsdam.de>
2022-07-01 11:01:50 +0200

    Correct GroupID mistake for consumer

--- person_dedup/person_consumer.py
+++ person_dedup/person_consumer.py
@@ -8,18 +8,17 @@ from build.gen.bakdata.rb.v1.rb_person_pb2 import RB_Person
 
 from general.consumer import consume_topic
 
+from person_dedup.constant import GROUP_ID
+
 log = logging.getLogger(__name__)
 
 class PersonConsumer:
 
     def consume(self):
-        msg_bafin_persons = consume_topic(TOPIC_BAFIN_PERSON, Bafin_Reportable_Person)
+        msg_bafin_persons = consume_topic(TOPIC_BAFIN_PERSON, Bafin_Reportable_Person, GROUP_ID)
         log.info("Bafin-Persons consumed.")
-        msg_rb_persons = consume_topic(TOPIC_RB_PERSON, RB_Person)
+        msg_rb_persons = consume_topic(TOPIC_RB_PERSON, RB_Person, GROUP_ID)
         log.info("HRB-Persons consumed.")
-
-        log.info(msg_bafin_persons)
-        log.info(msg_rb_persons)
 
         # Example for using the values:
         # print(str(msg_bafin_events[0].issuer))
