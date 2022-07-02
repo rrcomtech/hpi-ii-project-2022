af2fd1e95cc1018c98dc4d89b1fcee80caacbe59
fabian <fabian.lange@uni-potsdam.de>
2022-07-02 10:19:24 +0200

    Produce Dup and DeDup Persons

--- person_dedup/person_dedup_extractor.py
+++ person_dedup/person_dedup_extractor.py
@@ -55,10 +55,8 @@ class PersonDedupExtractor:
                     msg = msg_bafin_persons[index]
                 else:
                     msg = msg_rb_persons[index - len(msg_bafin_persons)]
-                person = self.personFromMessage(msg)
+                person = self.dedupPersonFromMessages([msg])
                 self.dedup_producer.produce_to_topic(person, hash(person.firstname.lower() + person.lastname.lower()))
-
-        print(duplicate_indices)
 
         duplicate_families = []
         for i, duplicate_index in enumerate(duplicate_indices):
@@ -82,15 +80,92 @@ class PersonDedupExtractor:
                             duplicate_indices[i + j + 1] = - duplicate_indices[i + j + 1]
             existing = False
             for existing_duplicate_family in duplicate_families:
-                if existing_duplicate_family[0] in duplicate_family:
+                if set(existing_duplicate_family) & set(duplicate_family):
                     existing = True
             if not existing:
                 duplicate_families.append(duplicate_family)
         print(duplicate_families)
+        for family in duplicate_families:
+            real_indices = []
+            msgs = []
+            for index in family:
+                real_indices.append(sorted_indices[index])
+            for index in real_indices:
+                msg = None
+                if index < len(msg_bafin_persons):
+                    msgs.append(msg_bafin_persons[index])
+                    msg = msg_bafin_persons[index]
+                else:
+                    msgs.append(msg_rb_persons[index - len(msg_bafin_persons)])
+                    msg = msg_rb_persons[index - len(msg_bafin_persons)]
+                dupPerson = self.dupPersonFromMessage(msg)
+                self.dup_producer.produce_to_topic(dupPerson, hash(dupPerson.firstname.lower() + dupPerson.lastname.lower()))
+            dedupPerson = self.dedupPersonFromMessages(msgs)
+            self.dedup_producer.produce_to_topic(dedupPerson, hash(dedupPerson.firstname.lower() + dedupPerson.lastname.lower()))
         exit(0)
 
-    def personFromMessage(self, msg):
+    def dedupPersonFromMessages(self, msgs):
         person = DeDup_Person()
+        for msg in msgs:
+            print(msg)
+            person.firstname = person.firstname if person.firstname and len(person.firstname) >= len(msg.firstname) else msg.firstname
+            person.lastname = person.lastname if person.lastname and len(person.lastname) >= len(msg.lastname) else msg.lastname
+            try:
+                person.title = person.title if person.title and len(person.title) >= len(msg.title) else msg.title
+            except:
+                pass
+            try:
+                person.bafin_issuer = person.issuer if person.issuer and len(person.issuer) >= len(msg.issuer) else msg.issuer
+            except:
+                pass
+            try:
+                person.rights_33_34 = person.rights_33_34 if person.rights_33_34 and len(person.rights_33_34) >= len(msg.rights_33_34) else msg.rights_33_34
+            except:
+                pass
+            try:
+                person.rights_38 = person.rights_38 if person.rights_38 and len(person.rights_38) >= len(msg.rights_38) else msg.rights_38
+            except:
+                pass
+            try:
+                person.rights_39 = person.rights_39 if person.rights_39 and len(person.rights_39) >= len(msg.rights_39) else msg.rights_39
+            except:
+                pass
+            try:
+                person.bafin_reportable_id = person.reportable_id if person.reportable_id and len(person.reportable_id) >= len(msg.reportable_id) else msg.reportable_id
+            except:
+                pass
+            try:
+                person.bafin_issuer_id = person.issuer_id if person.issuer_id and len(person.issuer_id) >= len(msg.issuer_id) else msg.issuer_id
+            except:
+                pass
+            try:
+                person.publishing_date = person.publishing_date if person.publishing_date and len(person.publishing_date) >= len(msg.publishing_date) else msg.publishing_date
+            except:
+                pass
+            try:
+                person.rb_corporateName = person.corporateName if person.corporateName and len(person.corporateName) >= len(msg.corporateName) else msg.corporateName
+            except:
+                pass
+            try:
+                person.birthdate = person.birthdate if person.birthdate and len(person.birthdate) >= len(msg.birthdate) else msg.birthdate
+            except:
+                pass
+            try:
+                person.city = person.city if person.city and len(person.city) >= len(msg.city) else msg.city
+            except:
+                pass
+            try:
+                person.rb_role = person.role if person.role and len(person.role) >= len(msg.role) else msg.role
+            except:
+                pass
+            try:
+                person.rb_corporateID = person.corporateID if person.corporateID and len(person.corporateID) >= len(msg.corporateID) else msg.corporateID
+            except:
+                pass
+        return person
+    
+    def dupPersonFromMessage(self, msg):
+        person = Dup_Person()
         person.firstname = msg.firstname
         person.lastname = msg.lastname
         try:
@@ -98,51 +173,23 @@ class PersonDedupExtractor:
         except:
             pass
         try:
-            person.bafin_issuer = msg.bafin_issuer
+            person.bafin_issuer = msg.issuer
         except:
             pass
         try:
-            person.rights_33_34 = msg.rights_33_34
+            person.bafin_reportable_id = msg.reportable_id
         except:
             pass
         try:
-            person.rights_38 = msg.rights_38
+            person.bafin_issuer_id = msg.issuer_id
         except:
             pass
         try:
-            person.rights_39 = msg.rights_39
+            person.rb_corporateName = msg.corporateName
         except:
             pass
         try:
-            person.bafin_reportable_id = msg.bafin_reportable_id
-        except:
-            pass
-        try:
-            person.bafin_issuer_id = msg.bafin_issuer_id
-        except:
-            pass
-        try:
-            person.publishing_date = msg.publishing_date
-        except:
-            pass
-        try:
-            person.rb_corporateName = msg.rb_corporateName
-        except:
-            pass
-        try:
-            person.birthdate = msg.birthdate
-        except:
-            pass
-        try:
-            person.city = msg.city
-        except:
-            pass
-        try:
-            person.rb_role = msg.rb_role
-        except:
-            pass
-        try:
-            person.rb_corporateID = msg.rb_corporateID
+            person.rb_corporateID = msg.corporateID
         except:
             pass
         return person
